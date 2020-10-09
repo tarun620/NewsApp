e73176368886f1119e1851ac7074f24534c92d24
tgsgkgvg <tarun.goel.620@gmail.com>
2020-10-09 14:38:32 +0530

    push

---
+++ app/src/main/java/com/maddy/newsdemo/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.maddy.newsdemo.util
+
+import com.maddy.newsdemo.R
+
+class Constants {
+    companion object {
+        const val PLACEHOLDER_TRANSPARENT_IMAGE = R.drawable.bg_transparent;
+        const val NEWS_API_KEY = "c7e0c06c41744d8089d5a8b716502fef";
+    }
+}
